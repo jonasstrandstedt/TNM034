3c9a4a47bb88f4dbe84e3e9020b01c559f87789e
julia freye <juifr807@student.liu.se>
2013-11-14 12:52:00 +0100

    added the function for getstraightenangle and implemendet in tnm034

--- tnm034.m
+++ tnm034.m
@@ -6,36 +6,33 @@ function strout = tnm034(Im)
 % 
 % strout: The resulting character string of the detected notes. 
 % The string must follow the pre-defined format, explained below. 
-% 
 % Your program code? 
 
-% gauss filter
-gauss =  fspecial('gaussian', 5, 0.9);
-
-% line detection filter (notes disappear)
-kernel = [ -1 -1 -1; 2 2 2; -1 -1 -1] / 9;
 
 % make grayscale image and get size
 grayscale = rgb2gray(Im);
 [x y]=size(grayscale);
 
-% blur the grayscale image
-blurred = conv2(grayscale,gauss,'same');
+level = graythresh(grayscale);
+BW = im2bw(grayscale, level);
 
-% threshold and invert the image
-Im_BW = im2bw(grayscale, 0.8);
-Im_BW = 1-Im_BW;
+%inverterar bilden
+BW = 1-BW;
 
-% use the line detection kernel
-C = conv2(Im_BW,kernel);
-C = im2bw(C, 0.3);
+%
+angle = getstraightenangle(BW);
+BW = imrotate(BW,angle,'bicubic','crop');
+% Rotate BW
+figure
+imshow(BW);
 
-% create black image and project the intensities to the left.
-R = zeros(x,y);
-for i=1:x
-    rowsum = sum(C(i,:));
-    R(i,1:rowsum) = ones(1, rowsum);
-end
+
+%create black image and project the intensities to the left.
+%R = zeros(x,y);
+%for i=1:x
+%    rowsum = sum(C(i,:));
+%    R(i,1:rowsum) = ones(1, rowsum);
+%end
 
 %figure
 %imshow(blurred);
@@ -43,8 +40,7 @@ end
 %imshow(Im_BW);
 %figure
 %imshow(C);
-figure
-imshow(R);
+%imshow(R);
 
 strout = 'TEST';
 end
