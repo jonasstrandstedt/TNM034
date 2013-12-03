c6b61ed6d680533db3c120637e1962b15260bbbf
julia freye <juifr807@student.liu.se>
2013-12-03 14:07:27 +0100

    fixed small codes issues

--- Image_processor/getstraightenangle.m
+++ Image_processor/getstraightenangle.m
@@ -4,7 +4,6 @@ function [ angle] = getstraightenangle( BW )
 % im Is BW, 
 % output is angle in degrees.
 
-
 thetaResolution = 0.01;
 rhoResolution = 1;
 
@@ -12,14 +11,13 @@ BW = bwmorph(BW, 'thin', Inf);
 BW = imrotate(BW, 90, 'bicubic','crop');
 [H, T, R]  = hough(BW, 'RhoResolution',rhoResolution, 'Theta',-10:thetaResolution:10);
 
-h = H/max(max(H));
+% for h is for vizualization.
+%h = H/max(max(H));
 
-peaks = houghpeaks(H)
+peaks = houghpeaks(H);
 
+angle = T(peaks(2));
 
-angle = T(peaks(2))
-
-%angle = tempangle*(sign(tempangle));
 
 end
 
